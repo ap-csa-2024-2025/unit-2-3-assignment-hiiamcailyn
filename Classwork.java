b326af1770cacb9abd2e49aa874a72e5cdb3dea2
hiiamcailyn <cailyntran@gmail.com>
2024-09-27 09:56:44 -0700

    yes

--- Classwork.java
+++ Classwork.java
@@ -2,7 +2,17 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    String x = new String("dictionary");
+    int length = x.length();
+    int n = 1;
+    firstLastN(x, n);
+  }
+
+  oublic static void firstLastN(String x, int n)
+  {
+    String strg1 = x.substring(0, n);
+    String strg2 = x.substring(length - n);
+    System.out.println()
   }
 
   // write solutions to problems below
