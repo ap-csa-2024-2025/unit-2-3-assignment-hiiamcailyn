162cc5d908e1231c2b4b755347301059e75cd889
hiiamcailyn <cailyntran@gmail.com>
2024-09-30 10:17:15 -0700

    hi

--- Classwork.java
+++ Classwork.java
@@ -6,29 +6,26 @@ public class Classwork
     int length = x.length();
     int n = 1;
     firstLastN(x, n);
+    delEnd(x, n);
+    
   }
 
-  oublic static void firstLastN(String x, int n)
+  public static void firstLastN(String x, int n)
   {
     String strg1 = x.substring(0, n);
-    String strg2 = x.substring(length - n);
-    System.out.println()
+    String strg2 = x.substring(x.length() - n);
+    System.out.println("apple");
   }
 
   // write solutions to problems below
 //DelEnd
-public static void main(String[] args)
-{
-  System.out.print("Have a nice day!");
-  String name = library;
-  int n = 5;
-}
 
 public static void delEnd(String x, int n)
 {
-  int stoppingIndex = s.length() - n;
-  String output = s.substring(0, stoppingIndex);
-  return output;
+  
+  int stoppingIndex = x.length() - n;
+  String output = x.substring(0, stoppingIndex);
+  System.out.print(output);
 }
 
 }
